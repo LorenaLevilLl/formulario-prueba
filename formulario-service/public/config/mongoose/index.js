885d867cc9a72da1a3b4d4453e761b972a8e6b00
LorenaLevilLl <lorena.levil.ll@gmail.com>
2021-02-13 18:57:19 -0300

    fix: se agrega archivo para pruebas

--- formulario-service/public/config/mongoose/index.js
+++ formulario-service/public/config/mongoose/index.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 //mongoose.connect('mongodb://localhost:27017/db-formulario');
 require('../../model/user');
 
-mongoose.connect('mongodb://mongo/db-formulario', {
+//mongoose.connect('mongodb://mongo/db-formulario', {
+mongoose.connect('mongodb://localhost:27017/db-formulario', {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
