39032d80dca3969bff8167f48746c2214e85ddce
Soportesdoimac <lclevil@sodimac.cl>
2021-02-15 10:17:52 -0300

    fix se actualiza conexion a mongo db

--- formulario-service/public/config/mongoose/index.js
+++ formulario-service/public/config/mongoose/index.js
@@ -4,8 +4,7 @@ const mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost:27017/db-formulario');
 require('../../model/user');
 
-// mongoose.connect('mongodb://mongo/db-formulario', {
-mongoose.connect('mongodb://127.0.0.1:27017/db-formulario', {
+mongoose.connect('mongodb://mongo/db-formulario', {
   useCreateIndex: true,
   useNewUrlParser: true,
   useUnifiedTopology: true,
